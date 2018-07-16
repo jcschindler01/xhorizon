e37dfab6eec89d911578b905942120a671587745
jcschindler01 <jcschindler01@gmail.com>
2018-07-15 23:15:21 -0700

    Change __init__ imports because of moving evap.

---
+++ xhorizon/evap/__init__.py
@@ -0,0 +1,2 @@
+
+from evap import *
--- xhorizon/shell_junction/__init__.py
+++ xhorizon/shell_junction/__init__.py
@@ -6,5 +6,3 @@ from active_slice_class import active_slice as aslice
 from passive_slice_class import passive_slice as pslice
 
 from helpers import set_ruv0, schwarz_dtr_masses
-
-from evap import accrete
