153857cd7771c8d47735d16147d9c93f14f68dd1
jcschindler01 <jcschindler01@gmail.com>
2018-12-06 19:02:01 -0800

    Experimented with 'ru' and 'rv' matchmodes. Seems like 'rv' always works, while 'ru' sometimes causes an error. I don't think it's a bug, this makes sense if the rhor = R approximation breaks down. Best to stick with 'rv' mode.

--- xhorizon/evap/evap.py
+++ xhorizon/evap/evap.py
@@ -2,7 +2,7 @@
 """
 This module provides method for making forming and evaporation BH diagrams.
 This module imports the entire xhorizon package. 
-It is meant for a higher level usage than the other subpackages, none of the
+It is meant for a higher level usage than the other subpackages, none of the 
 guts of xhorizon rely on this.
 """
 
