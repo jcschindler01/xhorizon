b0aaeae67ea6c97cffb029c6108d98f1bce94848
Joe Schindler <jcschindler01@gmail.com>
2018-12-13 19:01:30 -0800

    Messed around with schwarztest. Doesn't look like the way I've been doing it will generally work. Uh oh.

--- xhorizon/evap/schwarztest.py
+++ xhorizon/evap/schwarztest.py
@@ -39,20 +39,61 @@ T = 10.
 M = 1.
 le = .1
 
-def F(r,m):
-	r[r<=2.*m] = np.nan
-	return r + 2.*m * np.log(np.abs(r/(2.*m) - 1.))
 
-def Rh(m):
+def F(r,m):
+	r = r*np.array([1.])
+	if np.any(r<=2.*m):
+		out = None
+	else:
+		out = r + 2.*m * np.log(np.abs(r - 2.*m))
+	return 1.*out
+
+def R(m):
 	return 2.*m
+
+def zero(m, mprev=1., dv=1., T=1., M=1., l=1.):
+	dv = 1.*dv
+	du = -(3.*T/M**3) * m**2 * (m-mprev)
+	drstar = F(R(m)+l,m) - F(R(mprev)+l,m)
+	#return dv-du-2.*drstar
+	return dv-2.*drstar, du
+
 
 
 m = np.nan * np.ones(Nevap+1)
 du = np.nan*m
 dv = np.nan*m
 
+
+m[0] = 1.*M
 dv[1:-1] = .1*T
-du[1.]
+
+
+
+for mprev in np.linspace(1.,0.,5)[:-1]:
+
+	M = .01
+
+	T  = 50.   *M
+	l  = .01   *M
+	dv = .5   *M
+	mp = mprev*M
+
+	params = dict(mprev=mp, dv=dv, T=T, M=M, l=l)
+	mm = np.linspace(0,mp,1001)
+	zz = zero(mm, **params)
+
+	plt.plot(mm, zz[0], 'r-')
+	plt.plot(mm, zz[1], 'b-')
+
+	plt.plot(mm, 0.*mm, 'k-')
+	plt.xlim(0,M)
+	plt.ylim(np.array([-1.,1.])*np.max(np.abs(plt.ylim())))
+	plt.show()
+
+
+
+
 
 
 
