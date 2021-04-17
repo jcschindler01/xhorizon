37a9dd4eebb36190f8914f8571f6b327e29d3d1c
jcschindler01 <jcschindler01@gmail.com>
2021-04-16 18:54:01 -0700

    Removed unwanted dead code from an example.

--- examples/002 - Fancier Example/002 - Fancier Example.py
+++ examples/002 - Fancier Example/002 - Fancier Example.py
@@ -20,23 +20,7 @@ f = xh.mf.reissner_nordstrom(M=0.5, Q=0.3)
 # Try changing the boolean keyword inputs to see effect. Or, try changing MAXreg (maximally extended) to EFreg (Eddington-Finklestein).
 reg = xh.reg.MAXreg(f, rlines=True, boundary=True, uvgrid=False)
 
-# Manually add additional curves to the plot. Easy to turn off by changing "if True" to "if False".
-if False:
-  # Radius and time values in Schwarzschild (t,r) coordinates, parameterized by s. 
-  s = np.linspace(-1,3,5001)
-  tt = 2.*np.sinh(s)
-  rr = 1.1*np.cosh(s)
-  # Create new "curve" object, load (t,r) values, set style.
-  c = xh.curve()
-  # Load (t,r) Schwarzschild coordinate values.
-  c.tr = np.array([tt,rr])
-  # Set style.
-  c.sty = dict(c='r', zorder=700, lw=1.5)
-  # Add curve to all blocks in region. Automatically not added if curve not in region, so the above curve will only appear in exterior regions for R=1 Schwarzschild. Input to add_curves_tr must be a list of curve objects.
-  for b in reg.blocks:
-    b.add_curves_tr([c])
-
-# Manually add additional curves to the plot.
+# Manually add some additional curves to the plot.
 ## Initialize list of curves to add.
 crvlist = []
 ## Let's add some lines of t=constant in the exterior region.
@@ -59,7 +43,7 @@ for t0 in T:
 for b in reg.blocks:
   b.add_curves_tr(crvlist)
 
-# Fill trapped regions (that is, blocks where sign of f is -1) with gray background.
+# Fill trapped blocks (that is, blocks where sign of f is -1) with gray background.
 for b in reg.blocks:
   if b.sgnf==-1.:
     b.fill(sty=dict(fc='k', alpha=0.2))
