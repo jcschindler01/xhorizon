6b6c8381631091a657e0b9a7923c49c1a215e5d5
jcschindler01 <jcschindler01@gmail.com>
2022-03-10 10:06:45 -0800

    Beginning rebuild on v2dev branch to update to Python3.

--- xhorizon/shell_junction/__init__.py
+++
@@ -1,8 +0,0 @@
-
-
-
-
-from active_slice_class import active_slice as aslice
-from passive_slice_class import passive_slice as pslice
-
-from helpers import set_ruv0, schwarz_dtr_masses, slicecheck
