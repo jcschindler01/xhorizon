d98527230e033b71ad3370ddad312a8c8fafc653
jcschindler01 <jcschindler01@gmail.com>
2022-03-10 13:20:22 -0800

    Bring over old imports to main package.

---
+++ xhorizon/__init__.py
@@ -0,0 +1,20 @@
+
+from diagram_tools.plotstyle import *
+
+from diagram_tools.curve_class import curve
+from diagram_tools.diagram_classes import diagram, region, block
+
+from tortoise.metfunc_tests import func_test
+
+import diagram_tools.region_factory as reg
+import tortoise.metfunc_factory as mf
+
+import diagram_tools.block_masks as mask
+import diagram_tools.curvemakers as cm
+import diagram_tools.block_fill as fill
+
+import shell_junction as junc
+import shell_junction.reg_corner_masks as cornermask
+
+import evap
+
