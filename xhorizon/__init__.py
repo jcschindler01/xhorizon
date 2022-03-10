3198185b1b769866559d09dfcacdd5487b36edb7
jcschindler01 <jcschindler01@gmail.com>
2022-03-10 14:09:20 -0800

    Set main init to import tortoise while working on tortoise.

--- xhorizon/__init__.py
+++ xhorizon/__init__.py
@@ -1,20 +1,27 @@
 
-from diagram_tools.plotstyle import *
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use("classic")
 
-from diagram_tools.curve_class import curve
-from diagram_tools.diagram_classes import diagram, region, block
+from . import tortoise
 
-from tortoise.metfunc_tests import func_test
 
-import diagram_tools.region_factory as reg
-import tortoise.metfunc_factory as mf
+#from diagram_tools.plotstyle import *
 
-import diagram_tools.block_masks as mask
-import diagram_tools.curvemakers as cm
-import diagram_tools.block_fill as fill
+#from diagram_tools.curve_class import curve
+#from diagram_tools.diagram_classes import diagram, region, block
 
-import shell_junction as junc
-import shell_junction.reg_corner_masks as cornermask
+#from tortoise.metfunc_tests import func_test
 
-import evap
+#import diagram_tools.region_factory as reg
+#import tortoise.metfunc_factory as mf
+
+#import diagram_tools.block_masks as mask
+#import diagram_tools.curvemakers as cm
+#import diagram_tools.block_fill as fill
+
+#import shell_junction as junc
+#import shell_junction.reg_corner_masks as cornermask
+
+#import evap
 
