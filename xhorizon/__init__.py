dfb13b5a84f012b07a18b18d26df3c4dea65be38
jcschindler01 <jcschindler01@gmail.com>
2022-03-11 08:57:55 -0800

    Added just enough convenience imports to make shell junction tests module run without error.

--- xhorizon/__init__.py
+++ xhorizon/__init__.py
@@ -8,6 +8,11 @@ from xhorizon import diagram_tools
 from xhorizon import shell_junction
 
 
+from xhorizon.tortoise import metfunc_factory as mf
+from xhorizon.diagram_tools import region_factory as reg
+from xhorizon.diagram_tools import curvemakers as cm
+
+
 #from diagram_tools.plotstyle import *
 
 #from diagram_tools.curve_class import curve
@@ -15,11 +20,7 @@ from xhorizon import shell_junction
 
 #from tortoise.metfunc_tests import func_test
 
-#import diagram_tools.region_factory as reg
-#import tortoise.metfunc_factory as mf
-
 #import diagram_tools.block_masks as mask
-#import diagram_tools.curvemakers as cm
 #import diagram_tools.block_fill as fill
 
 #import shell_junction as junc
