f7c474727a68e21cee50e9645c042215738e1a62
jcschindler01 <jcschindler01@gmail.com>
2022-03-11 12:16:28 -0800

    Added back in more of the convenience name imports from the old version. A bit cluttered in the namespace, but better to be backward compatible.

--- xhorizon/__init__.py
+++ xhorizon/__init__.py
@@ -1,30 +1,34 @@
 
+## external
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use("classic")
 
+## main subpackages
 from xhorizon import tortoise
 from xhorizon import diagram_tools
 from xhorizon import shell_junction
 
+## convenience names
 from xhorizon.tortoise import metfunc_factory as mf
 from xhorizon.diagram_tools import region_factory as reg
 from xhorizon.diagram_tools import curvemakers as cm
 
+## alternate sub names
+from xhorizon import shell_junction as junc
+
+## more convenience names
+from xhorizon.diagram_tools.plotstyle import newfig, pubrc
+from xhorizon.diagram_tools.curve_class import curve
+from xhorizon.diagram_tools.diagram_classes import diagram, region, block
+from xhorizon.tortoise.metfunc_tests import func_test
+
+## more convenience names
+from xhorizon.diagram_tools import block_masks as mask
+from xhorizon.diagram_tools import block_fill as fill
+from xhorizon.shell_junction import reg_corner_masks as cornermask
+
+
+## evap subpackage needs to be loaded after the above due to circular import issue
 from xhorizon import evap
-
-#from diagram_tools.plotstyle import *
-
-#from diagram_tools.curve_class import curve
-#from diagram_tools.diagram_classes import diagram, region, block
-
-#from tortoise.metfunc_tests import func_test
-
-#import diagram_tools.block_masks as mask
-#import diagram_tools.block_fill as fill
-
-#import shell_junction as junc
-#import shell_junction.reg_corner_masks as cornermask
-
-#import evap
 
